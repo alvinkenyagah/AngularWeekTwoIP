c468da3ae828bb245997e031a68aad68be39eca9
Alvinkenyagah <alvin47k@gmail.com>
2022-04-21 07:51:26 +0300

    update index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ProfileService } from './services/profile.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,10 +13,13 @@ import { ProfileComponent } from './components/profile/profile.component';
   declarations: [
     AppComponent,
     ProfileComponent
+    
   ], 
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
+    
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,21 +1,67 @@
-import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+// import { Injectable } from '@angular/core';
+// import { HttpHeaders } from '@angular/common/http';
+// import 'rxjs/add/operator/map';
 
-@Injectable()
+// @Injectable()
+// export class ProfileService {
+
+//   private username:string;
+//   private clientid = '1e609537e8a85ccbfbd2';
+//   private clientsecret = '73052f3d7cadf2246794b9e6f0591ce7114fc125';
+
+//   constructor(private http:Http) {
+//      console.log("SERVICES IS NOW READY");
+//      this.username = 'kirandash';
+//   }
+//   getProfileInfo(){
+//   	return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+//   	.map((res: { json: () => any; }) => res.json());
+//   }  
+//   getProfileRepos(){
+//   	return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+//   	.map((res: { json: () => any; }) => res.json());
+//   }
+
+//   updateProfile(username:string){
+//   	this.username = username;
+
+//   }
+// }
+ 
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+   
+})
+
 export class ProfileService {
 
   private username:string;
   private clientid = '1e609537e8a85ccbfbd2';
   private clientsecret = '73052f3d7cadf2246794b9e6f0591ce7114fc125';
 
-  constructor(private http:Http) {
-     console.log("SERVICES IS NOW READY");
-     this.username = 'kirandash';
-  }
-   getProfileInfo(){
-     return this.http.get("https://api.github.com/users/" + this.username + "?client_id" + this.clientid + "&client_secret=" + this.clientid )
-     .map(res => res.json());
-   }  
+  constructor(private http:HttpClient) { 
+  	console.log("SERVICES IS NOW READY");
+  	this.username = '';
   }
 
+  getProfileInfo(){
+  	return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());
+  }
+
+  getProfileRepos(){
+  	return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());
+  }
+
+  updateProfile(username:string){
+  	this.username = username;
+  }
+
+}
