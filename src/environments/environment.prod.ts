3cb04debc3a15f95fc3ca9425022ca55672a3078
Alvinkenyagah <alvin47k@gmail.com>
2022-05-11 07:43:33 +0300

    update enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'https://api.github.com/users/',
+  apiKey:'Z2hwX1pNeGxlQVl2ME9tY20wY3d3dnVVNUVwcGJqejBsZjJRa0Q4RA=='
 };
